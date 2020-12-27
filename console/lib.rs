25ef430c191537fca80169632246c26c804e16db
Mnimi Aionios <mechild02@gmail.com>
2020-12-27 04:29:35 -0500

    [CLEAN] reorganized sources.

--- console/lib.rs
+++ console/lib.rs
@@ -1,7 +1,17 @@
 #![deny(clippy::all)]
 #![no_std]
+#![feature(
+	decl_macro
+)]
 /*!*/
 
-pub(crate) mod color;
+#[macro_use]
+extern crate lazy_static;
+extern crate volatile;
+
+pub(crate) mod colour;
 pub(crate) mod text;
 pub(crate) mod write;
+
+pub use self::colour::{Colour, ColourCode};
+pub use self::write::{_print, print, println};
