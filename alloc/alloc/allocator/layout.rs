f3ce13fc962c078eef19aac55d0742a30f043fb5
Mnimi Aionios <mechild02@gmail.com>
2020-12-28 04:08:59 -0500

    [ALLOC] added "GlobalAlloc" trait and "Layout" struct.

---
+++ alloc/alloc/allocator/layout.rs
@@ -0,0 +1,35 @@
+use core::mem::{size_of, align_of};
+
+pub struct Layout
+{
+	size: usize,
+	align: usize,
+}
+
+impl Layout
+{
+	#[inline]
+	pub fn new(size: usize) -> Self
+	{
+		Layout {
+			size,
+			align: 4,
+		}
+	}
+
+	#[inline]
+	pub fn from_type<T>() -> Self
+	{
+		Layout {
+			size: size_of::<T>(),
+			align: align_of::<T>(),
+		}
+	}
+
+	#[inline]
+	pub fn align_up(&self, i: usize) -> usize
+	{
+		let p = i + self.align - 1;
+		return p - (p % self.align);
+	}
+}
