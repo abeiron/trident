1aac8c55e11725e60fc2f4503cbc949e08511b0a
Mnimi Aionios <mechild02@gmail.com>
2020-12-29 09:42:22 -0500

    [FMT] fixed formatting.

--- alloc/alloc/allocator.rs
+++ alloc/alloc/allocator.rs
@@ -1,14 +1,14 @@
 //! `Allocator` type implementation.
 
 pub use self::layout::Layout;
-use self::linked_list::LinkedListAllocator;
 use alloc::alloc::{GlobalAlloc, Layout};
-use core::ptr::null_mut;
-use linked_list_allocator::LockedHeap;
 use core::ffi::c_void;
 use core::mem::size_of;
+use core::ptr::null_mut;
+use linked_list::LinkedListAllocator;
+use linked_list_allocator::LockedHeap;
 
-use core::ptr::{NonNull, read_unaligned, write_unaligned};
+use core::ptr::{read_unaligned, write_unaligned, NonNull};
 
 pub mod global;
 pub mod heap;
@@ -20,24 +20,30 @@ pub const HEAP_START: usize = 0x_4444_4444_0000;
 pub const HEAP_SIZE: usize = 100 * 1024; // 100 KiB
 
 /// A wrapper around spin::Mutex to permit trait implementations.
-pub struct Locked<A> {
+pub struct Locked<A>
+{
   inner: spin::Mutex<A>,
 }
 
-impl<A> Locked<A> {
-  pub const fn new(inner: A) -> Self {
+impl<A> Locked<A>
+{
+  pub const fn new(inner: A) -> Self
+  {
     Locked {
       inner: spin::Mutex::new(inner),
     }
   }
 
-  pub fn lock(&self) -> spin::MutexGuard<A> {
+  pub fn lock(&self) -> spin::MutexGuard<A>
+  {
     sefl.inner.lock()
   }
 }
 
 /// Align the given address 'addr' upwards to alignment 'align'.
-fn align_up(addr: usize, align: usize) -> usize {
+#[inline]
+pub fn align_up(addr: usize, align: usize) -> usize
+{
   let remainder = addr % align;
   if remainder == 0 {
     addr // addr already aligned
@@ -55,18 +61,15 @@ pub unsafe trait GlobalAlloc
   {
     let actual_size = layout.size + layout.align - 1 + size_of::<usize>();
 
-    let ptr = match self.alloc(Layout::new(actual_size))
-    {
+    let ptr = match self.alloc(Layout::new(actual_size)) {
       Some(p) => p as usize,
       None => return None,
     };
 
     let aligned_ptr = layout.align_up(ptr + size_of::<usize>());
-    let actual_ptr_ptr  = aligned_ptr - size_of::<usize>();
+    let actual_ptr_ptr = aligned_ptr - size_of::<usize>();
 
     write_unaligned(actual_ptr_ptr as *mut usize, ptr);
-
-
 
     Some(NonNull::new(aligned_ptr as *mut c_void).unwrap())
   }
@@ -80,4 +83,3 @@ pub unsafe trait GlobalAlloc
     self.dealloc(actual_ptr as *mut c_void);
   }
 }
-
--- alloc/alloc/allocator/global.rs
+++ alloc/alloc/allocator/global.rs
@@ -1,1 +1,7 @@
 /*TODO: Implement a global allocator.*/
+
+use super::GlobalAlloc;
+
+pub struct Global;
+
+impl GlobalAlloc for Global {}
