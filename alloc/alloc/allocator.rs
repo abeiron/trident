a7705a675642a9f4089f14db914d6c2cf2d95971
Mnimi Aionios <mechild02@gmail.com>
2020-12-29 09:26:23 -0500

    [ALLOC] removed reference to x86_64 crate.

--- alloc/alloc/allocator.rs
+++ alloc/alloc/allocator.rs
@@ -1,30 +1,14 @@
 //! `Allocator` type implementation.
 
 pub use self::layout::Layout;
+use self::linked_list::LinkedListAllocator;
 use alloc::alloc::{GlobalAlloc, Layout};
 use core::ptr::null_mut;
-use linked_list::LinkedListAllocator;
 use linked_list_allocator::LockedHeap;
 use core::ffi::c_void;
 use core::mem::size_of;
-<<<<<<< HEAD
-use core::ptr::{NonNull, read_unaligned, write_unaligned};
-use x86_64::{
-    structures::paging::{
-      mapper::MapToError, FrameAllocator, Mapper, page, PageTableFlags, Size4KiB,
-    },
-    VirtAddr,
-};
 
-#[global_allocator]
-static Allocator: Locked<LinkedListAllocator> =
-    Locked::new(LinkedListAllocator::new());
-#[global_allocator]
-static ALLOCATOR: LockedHeap = LockedHeap::empty();
-
-=======
 use core::ptr::{NonNull, read_unaligned, write_unaligned};
->>>>>>> fac3c4af15055b74932574380d89a78d5e08b5da
 
 pub mod global;
 pub mod heap;
@@ -60,36 +44,6 @@ fn align_up(addr: usize, align: usize) -> usize {
   } else {
     addr - remainder + align
   }
-}
-
-pub fn init_heap(
-    mapper: &mut impl Mapper<Size4KiB>,
-    frame_allocator: &mut impl FrameAllocator<Size4KiB>,
-) -> Result<(), MapToError<Size4KiB>> {
-  // [...] map all heap pages to physical frames
-  let page_range = {
-    let heap_start = VirtAddr::new(HEAP_START as u64);
-    let heap_end = heap_start + HEAP_SIZE - lu64;
-    let heap_start_page = Page::containing_address(heap_start);
-    let heap_end_page = Page::containing_address(heap_end);
-    Page::range_inclusive(heap_start_page, heap_end_page)
-  };
-
-  for page in page_range {
-    let frame = frame_allocator
-        .allocate_frame()
-        .ok_or(MapToError::FrameAllocationFailed)?;
-    let flags = PageTableFlags::PRESENT | PageTableFlags::WRITABLE;
-    unsafe {
-      mapper.map_to(page, frame, flags, frame_allocator)?.flush()
-    };
-  }
-  // new
-  unsafe {
-    ALLOCATOR.lock().init(HEAP_START, HEAP_SIZE);
-  }
-
-  Ok(())
 }
 
 pub unsafe trait GlobalAlloc
